2c0312102c2cb0c30e652a7f73c275efc0841203
Leonardo Holanda <nardoso@gmail.com>
2022-02-03 23:39:22 -0300

    Testando integração com jUnit e Cucumber Options.

--- src/test/java/testesCucumber/AprenderCucumber.java
+++ src/test/java/testesCucumber/AprenderCucumber.java
@@ -1,29 +1,21 @@
 package testesCucumber;
 
-import io.cucumber.java.PendingException;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.cucumber.core.cli.Main;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Quando;
+import io.cucumber.java.pt.Então;
 
 public class AprenderCucumber {
 
-	@Given("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Dado("que criei o arquivo corretamente")
+	public void que_criei_o_arquivo_corretamente() { 
 	}
 
-	@When("^executá-lo$")
-	public void executá_lo() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Quando("executá-lo")
+	public void executá_lo() { 
 	}
 
-	@Then("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Então("a especificação deve finalizar com sucesso")
+	public void a_especificação_deve_finalizar_com_sucesso() { 
 	}
 	
 }
