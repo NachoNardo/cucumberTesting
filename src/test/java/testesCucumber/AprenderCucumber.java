b25ece9ecec8fe476b109631e955cda52dd45e89
Leonardo Holanda <nardoso@gmail.com>
2022-02-03 23:48:03 -0300

    Testando asserts com jUnit.

--- src/test/java/testesCucumber/AprenderCucumber.java
+++ src/test/java/testesCucumber/AprenderCucumber.java
@@ -18,4 +18,22 @@ public class AprenderCucumber {
 	public void a_especificação_deve_finalizar_com_sucesso() { 
 	}
 	
+	private int contador = 0;
+	
+	@Dado("que o valor do contador é {int}")
+	public void que_o_valor_do_contador_é(Integer int1) { 
+		contador = int1;
+	}
+
+	@Quando("eu incrementar em {int}")
+	public void eu_incrementar_em(Integer int1) { 
+		contador += int1;
+	}
+
+	@Então("o valor do contador será {int}")
+	public void o_valor_do_contador_será(Integer int1) { 
+		System.out.println(int1==contador); 
+	}
+	
+	
 }
--- src/test/java/testesCucumber/Runner.java
+++ src/test/java/testesCucumber/Runner.java
@@ -2,6 +2,7 @@ package testesCucumber;
 
 import org.junit.runner.RunWith;
 
+import io.cucumber.core.snippets.SnippetType;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
