9b0531860995da1c141e84c678c0dcd42611f9b4
Leonardo Holanda <nardoso@gmail.com>
2022-02-03 23:13:54 -0300

    Corrigindo a versão da dependência do Cucumber no Maven.

---
+++ src/test/java/testesCucumber/AprenderCucumber.java
@@ -0,0 +1,29 @@
+package testesCucumber;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.core.cli.Main;
+
+public class AprenderCucumber {
+
+	@Given("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^executá-lo$")
+	public void executá_lo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
